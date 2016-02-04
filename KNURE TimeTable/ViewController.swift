364d3bb10a2dd720ad6d1bea304ed25652090bff
Xedart <Xedpasmail@gmail.com>
2016-02-04 16:14:54 +0200

    Initial Commit

---
+++ KNURE TimeTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KNURE TimeTable
+//
+//  Created by Shkil Artur on 2/4/16.
+//  Copyright © 2016 Shkil Artur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
