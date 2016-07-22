63674a759a3c54ba5553fef71b6f669d994f0a35
Xedart <Xedpasmail@gmail.com>
2016-07-22 11:53:53 +0300

    Pre-framework cganges commit

---
+++ Today schedule/TodayViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TodayViewController.swift
+//  Today schedule
+//
+//  Created by Shkil Artur on 7/21/16.
+//  Copyright © 2016 Shkil Artur. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+        
+        
+    }
+    
+    func widgetPerformUpdate(completionHandler: ((NCUpdateResult) -> Void)) {
+        
+        
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
